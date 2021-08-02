2638f71e690d4befb309074e7c4e11cd1f394969
R0CKSTAR <yeahdongcn@gmail.com>
2021-08-02 15:22:52 +0800

    Remove footer

--- config/footer.js
+++ config/footer.js
@@ -1,12 +1,3 @@
 export function options(pl, issues) {
-  if (!issues) {
-    issues = 'https://github.com/rancher/dashboard/issues/new';
-  }
-
-  return {
-    'footer.docs':   'https://docs.harvesterhci.io/',
-    'footer.forums': 'https://forums.rancher.com/',
-    'footer.slack':  'https://slack.rancher.io',
-    'footer.issue':  'https://github.com/harvester/harvester/issues/new/choose',
-  };
+  return {};
 }
