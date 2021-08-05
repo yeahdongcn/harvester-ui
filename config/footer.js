34f401350389f6100970d2073e5a94005c5c8f45
R0CKSTAR <yeahdongcn@gmail.com>
2021-08-05 10:36:04 +0800

    Combine all changes

--- config/footer.js
+++ config/footer.js
@@ -1,12 +1,3 @@
 export function options(pl) {
-  if ( pl !== 'rancher' ) {
-    return {};
-  }
-
-  return {
-    'footer.docs':   'https://github.com/harvester/harvester/tree/master/docs',
-    'footer.forums': 'https://forums.rancher.com/',
-    'footer.slack':  'https://slack.rancher.io',
-    'footer.issue':  'https://github.com/harvester/harvester/issues/new/choose',
-  };
+  return {};
 }
